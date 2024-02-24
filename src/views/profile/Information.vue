e351e542d0ead53962c41adba868ab14f8de6164
LeoKu <leokudev@gmail.com>
2024-02-24 11:00:00 +0800

    style

--- src/views/profile/Information.vue
+++ src/views/profile/Information.vue
@@ -20,34 +20,34 @@
 
 <script>
 export default {
-  name: "Information",
+  name: 'Information',
 
   data() {
     return {
       form: this.$form.createForm(this),
       formItems: [
         {
-          label: "账号",
-          placeholder: "请输入账号",
-          decorator: ["username", { initialValue: "admin@magic.com" }],
+          label: '账号',
+          placeholder: '请输入账号',
+          decorator: ['username', { initialValue: 'admin@magic.com' }],
         },
         {
-          label: "昵称",
-          placeholder: "请输入昵称",
-          decorator: ["nickname", { initialValue: "令狐少侠" }],
+          label: '昵称',
+          placeholder: '请输入昵称',
+          decorator: ['nickname', { initialValue: '令狐少侠' }],
         },
         {
-          label: "邮箱",
-          placeholder: "请输入邮箱",
-          decorator: ["email", { initialValue: "leokudev@gmail.com" }],
+          label: '邮箱',
+          placeholder: '请输入邮箱',
+          decorator: ['email', { initialValue: 'leokudev@gmail.com' }],
         },
         {
-          label: "角色",
-          placeholder: "请输入角色",
-          decorator: ["role", { initialValue: "开发者" }],
+          label: '角色',
+          placeholder: '请输入角色',
+          decorator: ['role', { initialValue: '开发者' }],
         },
       ],
-    };
+    }
   },
-};
+}
 </script>
